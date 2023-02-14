2e8c34de7037d713527c9f950ad7bd47bd5fb555
Shubh Jaiswal <112515178+shubhjaiswal02@users.noreply.github.com>
2023-02-14 22:24:26 +0530

    leetcode 852

---
+++ Leetcode_Ques/Binary_Search/peak_index_mountain_array.java
@@ -0,0 +1,33 @@
+/*
+Example 1:
+
+Input: arr = [0,1,0]
+Output: 1
+Example 2:
+
+Input: arr = [0,2,1,0]
+Output: 1
+Example 3:
+
+Input: arr = [0,10,5,2]
+Output: 
+*/
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid+1]) {
+                
+                end = mid;
+            } else {
+                
+                start = mid + 1; 
+            }
+        }
+        return start;
+    }
+}
